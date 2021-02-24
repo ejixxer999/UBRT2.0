6491c03a62196c40d948776d18a68d7db458447a
ejixxer999 <lehman.ethan2@gmail.com>
2021-02-23 22:51:23 -0500

    added validation for uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true
+    validate :email, uniqueness: true
     has_many :jobsites
     has_many :companies, through: :jobsites
 
