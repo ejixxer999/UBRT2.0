55f86463d0198f9eea10a1e13042108132829cd6
ejixxer999 <lehman.ethan2@gmail.com>
2021-03-05 22:44:28 -0500

    added validation for company_id, and company_attributes

--- app/models/jobsite.rb
+++ app/models/jobsite.rb
@@ -3,6 +3,13 @@ class Jobsite < ApplicationRecord
     belongs_to :company
     validates :name, presence: true
     validates :location, presence: true
+    validates :company_id, presence: true
+    validates_associated :company
+
+    def company_attributes=(attributes)
+        company = Company.find_or_create_by(attributes)
+        self.company = company if company.valid? || !self.company
+    end 
 
 
 end
